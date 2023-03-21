f1c18796b129ad47466e444cf41607cfacdc3622
alebrito2 <alexsanderbrito123@gmail.com>
2023-03-21 08:25:53 -0300

    Classe: Position

--- src/chessMatch/Program.java
+++ src/chessMatch/Program.java
@@ -1,9 +1,14 @@
 package chessMatch;
 
+import boardchess.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Position pos = new Position(2, 5);
+		
+		System.out.println(pos);
 
 	}
 
